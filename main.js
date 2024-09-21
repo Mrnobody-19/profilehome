163a3a80192eb0f3cd7863016bd6cc0820dc3232
Mrnobody-19 <kalejaiyeabdulsamad4@gmail.com>
2024-09-21 18:21:34 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+var typed = new Typed(".text", {
+    strings: [ " Web Designer", "Bug Bounter", "Ethical Hacker", "Penetration Tester", "Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+
+var typed = new Typed(".logo", {
+    strings: ["Kalejaiye's Portfolio"],
+    typeSpeed:100,
+    backSpeed:500,
+    backDelay:1000,
+    loop:true
+});
+
